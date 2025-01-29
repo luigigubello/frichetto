d9c72847a755a0cdcecde4bccc06540566ff763b
valclclcl <val.collavini@gmail.com>
2025-01-29 22:11:00 +0100

    whiled all the fors

--- helloworld.cpp
+++ helloworld.cpp
@@ -11,21 +11,45 @@ int main()
     cout << "Type a number: ";
     cin >> x;
     // cout << "Your number is: " << x;
-    for (int i = 1; i <= x; i++) // User input defines number of lines
+    // for (int i = 1; i <= x; i++) // User input defines number of lines
+    int i = 1;
+    while (i <= x)
     {
         if (i == 1 || i == x) // Print a line of x stars at the beginning and at the end
-        { 
-            for (int j = 1; j <= x; j++)
+        {
+            int j = 1;
+            while (j <= x)
             {
                 cout << "*";
+                ++j;
             }
+            /*for (int j = 1; j <= x; j++)
+            {
+                cout << "*";
+            }*/
 
             cout << endl;
         }
 
         else // Print a star for every 1 and i and print a space for every other value
         { 
-            for (int j = 1; j <= x; j++)
+            int j = 1;
+            while (j <= x)
+            {
+                if (j == 1 || j == x)
+                {
+                    cout << "*";
+                }
+
+                else
+                {
+                    cout << " ";
+                }
+
+                ++j;
+            }
+
+            /*for (int j = 1; j <= x; j++)
             {
                 if (j == 1 || j == x)
                 {
@@ -37,9 +61,10 @@ int main()
                     cout << " "; // E INVECE DIO CANE NO
                 }
 
-            }
+            } */
 
             cout << endl;
         }
+    ++i;
     }
 }
